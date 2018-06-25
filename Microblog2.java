f4868738c31370f4b457d4a5dd1bd3bb31774e63
ssnewby <35549468+ssnewby@users.noreply.github.com>
2018-06-25 21:30:25 +0000

    Add files via upload

--- Microblog2.java
+++ Microblog2.java
@@ -6,13 +6,14 @@ import java.util.Scanner;
 public class Microblog2 {
 
     // initialize variables
-    static Scanner keyboard = new Scanner(System.in);
+    static Scanner intkeyboard = new Scanner(System.in);
+    static Scanner stringkeyboard = new Scanner(System.in);
     int item;
 
 
     static ArrayList<User> existingUsers = new ArrayList<>();
     private User postedUser;
-    static ArrayList<Postedinfo> Postedinfo = new ArrayList<>();
+    private static ArrayList<Postedinfo> Postedinfo = new ArrayList<>();
     static int orderPosted;
 
 
@@ -29,80 +30,92 @@ public class Microblog2 {
             System.out.println("3) Create a post as the current user");
             System.out.println("4) Print all posts");
             System.out.println("5) Print all users");
-            menuChoice = keyboard.nextInt();
+            menuChoice = intkeyboard.nextInt();
 
             if (menuChoice == 1) {
                 newUser();
             } else if (menuChoice == 2) {
                 existingUsers();
             } else if (menuChoice == 3) {
-                currentUsers();
+                createPost();
             } else if (menuChoice == 4) {
                 allPosts();
             } else if (menuChoice == 5) {
                 allUsers();
             }
         }
-            return menuChoice;
-        }
-
-
-
-        public void newUser() {
-            System.out.println("What is your webAddress");
-            String webAddress = keyboard.nextLine();
-            System.out.println("What is your userName");
-            String userName = keyboard.nextLine();
-            System.out.println("What is your emailAddress");
-            String emailAddress = keyboard.nextLine();
-            System.out.println("What is your firstName");
-            String firstName = keyboard.nextLine();
-            System.out.println("What is your lastName");
-            String lastName = keyboard.nextLine();
-
-            User newUser = new User(webAddress, userName, emailAddress, firstName, lastName);
-            existingUsers.add(newUser);
-        }
-
-        public void existingUsers () {
-            System.out.println("Become an existing user");
-            for (int j = 0; j < existingUsers.size(); j++)
-
-            {
-                System.out.println(j+ " " + existingUsers.get(j).getuserName());
-
-            }
-
-
-            int selectedIndex = keyboard.nextInt();
-
-            postedUser = existingUsers.get(selectedIndex);
-        }
-
-         public void currentUsers () {
-            System.out.println("What do you want to post?");
-            String contentPosted = keyboard.nextLine();
-            System.out.println("What is the site address?");
-            String siteAddress = keyboard.nextLine();
-
-            Postedinfo newPost = new Postedinfo(postedUser, orderPosted,contentPosted,siteAddress);
-        }
-
-          public void allPosts () {
-              System.out.println("Print all posts");
-              for (int j = 0; j < Postedinfo.size(); j++) {
-                  System.out.println(j + " " + Postedinfo.get(j).getcontentPosted());
-              }
-          }
-            public void allUsers (){
-                System.out.println("Print all users");
-                for (int j = 0; j < existingUsers.size(); j++) {
-                    System.out.println(j + " " + existingUsers.get(j).getuserName());
-                }
-            }
-
+        return menuChoice;
     }
 
+
+    public void newUser() {
+        System.out.println("What is your webAddress");
+        String webAddress = stringkeyboard.nextLine();
+        System.out.println("What is your userName");
+        String userName = stringkeyboard.nextLine();
+        System.out.println("What is your emailAddress");
+        String emailAddress = stringkeyboard.nextLine();
+        System.out.println("What is your firstName");
+        String firstName = stringkeyboard.nextLine();
+        System.out.println("What is your lastName");
+        String lastName = stringkeyboard.nextLine();
+
+        User newUser = new User(webAddress, userName, emailAddress, firstName, lastName);
+        existingUsers.add(newUser);
+    }
+
+    public void existingUsers() {
+        System.out.println("Become an existing user");
+        for (int j = 0; j < existingUsers.size(); j++)
+
+        {
+            System.out.println(j + " " + existingUsers.get(j).getuserName());
+
+        }
+
+
+        int selectedIndex = intkeyboard.nextInt();
+
+        postedUser = existingUsers.get(selectedIndex);
+    }
+
+    public void createPost() {
+        System.out.println("What do you want to post?");
+        String contentPosted = stringkeyboard.nextLine();
+        System.out.println("What is the site address?");
+        String siteAddress = stringkeyboard.nextLine();
+
+        Postedinfo newPost = new Postedinfo(postedUser, orderPosted, contentPosted, siteAddress);
+        Postedinfo.add(newPost);
+    }
+
+    public void allPosts() {
+        System.out.println("Print all posts");
+        for (int j = 0; j < Postedinfo.size(); j++) {
+            System.out.println(j + " " + Postedinfo.get(j).getcontentPosted());
+        }
+    }
+
+    public void allUsers() {
+        System.out.println("Print all users");
+        for (int j = 0; j < existingUsers.size(); j++) {
+            System.out.println(j + " " + existingUsers.get(j).getuserName());
+        }
+    }
+
+
+    private String getlatestPost(User postedUser) {
+        String getlatestPost = "";
+        for (int j = 0; j < Postedinfo.size(); j++) {
+            if (postedUser == Postedinfo.get(j).getpostedUser()) {
+                getlatestPost = Postedinfo.get(j).getcontentPosted();
+            }
+        }
+        return getlatestPost;
+    }
+
+
+}
 
 
 
